50b1599f09eb55ba70c82bc80417ecdc28ccd41a
Psyduck_20 <66348567+slayerzeroa@users.noreply.github.com>
2026-02-06 23:56:46 +0900

    feat: gh-pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { HashRouter } from "react-router-dom";
+// import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+// // styles for this kit
+// import "assets/css/bootstrap.min.css";
+// import "assets/scss/now-ui-kit.scss?v=1.5.0";
+// import "assets/demo/demo.css?v=1.5.0";
+// import "assets/demo/nucleo-icons-page-styles.css?v=1.5.0";
+// // pages for this kit
+// import Index from "pages/Index.js";
+// import NucleoIcons from "views/NucleoIcons.js";
+// import LoginPage from "views/examples/LoginPage.js";
+// import LandingPage from "views/examples/LandingPage.js";
+// import ProfilePage from "views/examples/ProfilePage.js";
+// import About from "pages/About";
+// import Projects from "pages/Projects";
+// import Members from "pages/Members";
+// import FE from "pages/FE";
+// import Contact from "pages/Contact";
+// import Activities from "pages/Activities";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/activities" element={<Activities />} />
+//       <Route path="/contact" element={<Contact />} />
+//       <Route path="/fe" element={<FE />} />
+//       <Route path="/members" element={<Members />} />
+//       <Route path="/projects" element={<Projects />} />
+//       <Route path="/index" element={<Index />} />
+//       <Route path="/about" element={<About />} />
+//       <Route path="/nucleo-icons" element={<NucleoIcons />} />
+//       <Route path="/landing-page" element={<LandingPage />} />
+//       <Route path="/profile-page" element={<ProfilePage />} />
+//       <Route path="/login-page" element={<LoginPage />} />
+//       <Route path="/" element={<Navigate to="/index" />} />
+//       <Route path="/projects/:id" element={<Projects />} />
+//     </Routes>
+//   </BrowserRouter>,
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
 // styles for this kit
 import "assets/css/bootstrap.min.css";
 import "assets/scss/now-ui-kit.scss?v=1.5.0";
 import "assets/demo/demo.css?v=1.5.0";
 import "assets/demo/nucleo-icons-page-styles.css?v=1.5.0";
+
 // pages for this kit
 import Index from "pages/Index.js";
 import NucleoIcons from "views/NucleoIcons.js";
@@ -23,21 +69,22 @@ import Activities from "pages/Activities";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/activities" element={<Activities />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/fe" element={<FE />} />
       <Route path="/members" element={<Members />} />
       <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/:id" element={<Projects />} />
       <Route path="/index" element={<Index />} />
       <Route path="/about" element={<About />} />
       <Route path="/nucleo-icons" element={<NucleoIcons />} />
       <Route path="/landing-page" element={<LandingPage />} />
       <Route path="/profile-page" element={<ProfilePage />} />
       <Route path="/login-page" element={<LoginPage />} />
-      <Route path="/" element={<Navigate to="/index" />} />
-      <Route path="/projects/:id" element={<Projects />} />
+      <Route path="/" element={<Navigate to="/index" replace />} />
+      <Route path="*" element={<Navigate to="/index" replace />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>,
 );
